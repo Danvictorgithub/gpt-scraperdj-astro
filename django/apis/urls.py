a287e953c0da020a6d742719dbe2894bcb98dc1c
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2024-07-09 22:14:30 +0800

    restarted backend

--- django/apis/urls.py
+++
@@ -1,8 +0,0 @@
-from django.http import JsonResponse
-from django.urls import path
-from .views import BookAPIView
-
-urlpatterns = [
-    path("books/",BookAPIView.as_view(),name="book_list"),
-    path("",lambda request : JsonResponse({"message":"Welcome to the API"})) 
-]
