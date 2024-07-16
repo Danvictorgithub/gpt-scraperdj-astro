1827145e4619f6dee575e49146a039e7dc9c9ebd
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2024-07-16 18:21:47 +0800

    added vercel

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,17 +3,16 @@ import tailwind from "@astrojs/tailwind";
 import auth from "auth-astro";
 import node from "@astrojs/node";
 import react from "@astrojs/react";
-
 import icon from "astro-icon";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   security: {
-    checkOrigin: true,
+    checkOrigin: true
   },
   integrations: [icon(), tailwind(), react(), auth()],
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel()
 });
