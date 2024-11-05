78ef52deb3d010bcdc293ef27ec836553cb86bd9
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2024-11-05 22:43:18 +0800

    updated gemini apis

--- backend/apis/serializers.py
+++ backend/apis/serializers.py
@@ -19,8 +19,8 @@ class ChatRequestGeminiSeralizer(serializers.Serializer):
     topic = serializers.CharField(max_length=200,required=False,default="general")
 
 class ChatRequestGeminiRandomSeralizer(serializers.Serializer):
-    max_prompt = serializers.IntegerField(max_value=70,default=30)
+    max_prompt = serializers.IntegerField(max_value=70,default=6)
     initial_message = serializers.CharField(max_length=4096,
                                             required=False,
                                             default=
-                                            "Lets pretend to have a basic conversation like a human with basic vocabulary, you can also pretend to have emotion, I would like you to limit the length of your response to small paragraphs, I will do the same. I repeat respond with short paragraph")
+                                            "Lets pretend to have a basic conversation like a human with basic vocabulary, you can also pretend to have emotion, I would like you to limit the length of your response to small paragraphs, I will do the same. I repeat respond with short paragraph, please limit to less than 300 character")
